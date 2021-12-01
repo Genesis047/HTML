f787782c147c9920bcd6a3e283851d79eca725b2
RYAN ANGELO DELA CRUZ/ <ryanangelo.delacruz@gsfe.tupcavite.edu.ph>
2021-12-01 14:14:13 +0800

    update jquery

--- javascript/issue.js
+++ javascript/issue.js
@@ -3,20 +3,32 @@ $(document).ready(function() {
 });
 
 $(document).ready(function(){
-   $("#issue_table").on('click','.btn',function(){
-           // get the current row
-           var currentRow=$(this).closest("tr"); 
-           
-           var col1=currentRow.find(".book_title").html(); // get current row 1st table cell TD value
-           var col2=currentRow.find(".book_author").html(); // get current row 2nd table cell TD value
-         
-           var data=col1+"\n"+col2;
-           window.alert(data);
-      });
-
-   });   
-
-
-$(document).ready(function(){
     $('[data-bs-toggle="tooltip"]').tooltip();   
+});
+
+
+$(document).ready(function() {
+
+  //$(".zz-modal").click(function() {
+  //  $("#con-close-modal").modal('show');
+  //});
+
+  $('#myModal').on('show.bs.modal', function(e) {
+      console.log(e);
+      console.log(e.relatedTarget);
+
+
+      var _button = $(e.relatedTarget); // Button that triggered the modal
+
+       // console.log(_button, _button.parents("tr"));
+      var _row = _button.parents("tr");
+      var _title = _row.find(".book_title").text();
+      var _author = _row.find(".book_author").text();
+      var _pubdate = _row.find(".exp_date").text();
+
+      $(this).find(".mbook_title").val(_title);
+      $(this).find(".mbook_author").val(_author);
+      $(this).find(".mexp_date").val(_pubdate);
+  });
+
 });
