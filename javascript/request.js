54bc28486f92feb4cb1b6986f382940ec261aed7
RYAN ANGELO DELA CRUZ/ <ryanangelo.delacruz@gsfe.tupcavite.edu.ph>
2021-12-04 10:13:51 +0800

    update student request

---
+++ javascript/request.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#request_table').DataTable();
+});
+ 
+
+$(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();   
+});
+
+
+$(document).ready(function() {
+
+  $('#myModal').on('show.bs.modal', function(e) {
+      console.log(e);
+      console.log(e.relatedTarget);
+
+
+      var _button = $(e.relatedTarget); // Button that triggered the modal
+
+       // console.log(_button, _button.parents("tr"));
+      var _row = _button.parents("tr");
+      var _title = _row.find(".book_title").text();
+      var _author = _row.find(".book_author").text();
+      var _name = _row.find(".name").text();
+      var _date_req = _row.find(".date_req").text();
+
+      $(this).find(".mbook_title").val(_title);
+      $(this).find(".mbook_author").val(_author);
+      $(this).find(".mname").val(_name);
+      $(this).find(".mreq_d").val(_date_req);
+  });
+
+});
+
+
+$(document).ready( function() {
+    var now = new Date();
+    var today = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
+    $('#issue_d').val(today);
+});
