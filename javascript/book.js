b985b4c5671bb4016f1817f3964f3e067219716b
RYAN ANGELO DELA CRUZ/ <ryanangelo.delacruz@gsfe.tupcavite.edu.ph>
2021-12-04 23:21:49 +0800

    ajax table

--- javascript/book.js
+++ javascript/book.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
     $('#book_table').DataTable();
 });
+ 
 
-
-function getElementBy()
-{
- elem = document.getElementById('my_id');
- text = elem.textContent;
- alert(text);
-
-}
 
  $(window).scroll(function(){
  $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
  });
 
-
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+    
