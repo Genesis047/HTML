d83e79573ca932b48668413d2133c1ba0e0c80a8
RYAN ANGELO DELA CRUZ/ <ryanangelo.delacruz@gsfe.tupcavite.edu.ph>
2021-12-04 21:15:22 +0800

    update home

--- javascript/book.js
+++ javascript/book.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $('#book_table').DataTable();
 });
+
+
+function getElementBy()
+{
+ elem = document.getElementById('my_id');
+ text = elem.textContent;
+ alert(text);
+
+}
+
+ $(window).scroll(function(){
+ $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
+ });
+
+
